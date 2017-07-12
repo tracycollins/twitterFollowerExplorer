a02928308ceff8f5c2db84b80918c0b887d02685
tracycollins <tc@threeceemedia.com>
2017-07-11 21:54:16 -0400

    mongoose update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,26 +1,32 @@
 var config = require('./config'),
   mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 module.exports = function() {
 
-  var options = {
-    server: {
-      auto_reconnect: true,
-      poolSize: 5,
-      reconnectTries: 14000,
-      socketOptions: {
-        // reconnectTries: 14000,
-        keepAlive: 1000,
-        socketTimeoutMS: 180000,
-        connectTimeoutMS: 180000
-      }
-    },
-    db: {
-      numberOfRetries: 1000,
-      retryMiliSeconds: 1000
-    }
+  // var options = {
+  //   server: {
+  //     auto_reconnect: true,
+  //     poolSize: 5,
+  //     reconnectTries: 14000,
+  //     socketOptions: {
+  //       // reconnectTries: 14000,
+  //       keepAlive: 1000,
+  //       socketTimeoutMS: 180000,
+  //       connectTimeoutMS: 180000
+  //     }
+  //   },
+  //   db: {
+  //     numberOfRetries: 1000,
+  //     retryMiliSeconds: 1000
+  //   }
+  // };
+
+  var options = { 
+    useMongoClient: true,
+    poolSize: 20,
+    promiseLibrary: global.Promise
   };
 
   var wordAssoDb = mongoose.connect(config.wordAssoDb, options, function(error) {
@@ -33,22 +39,22 @@ module.exports = function() {
 
   // CONNECTION EVENTS
   // When successfully connected
-  wordAssoDb.connection.on('connected', function() {
-    console.log('MONGOOSE default connection OPEN to ' + config.wordAssoDb);
-  });
+  // wordAssoDb.connection.on('connected', function() {
+  //   console.log('MONGOOSE default connection OPEN to ' + config.wordAssoDb);
+  // });
 
-  wordAssoDb.connection.on('close', function() {
-    console.log('MONGOOSE default connection CLOSED to ' + config.wordAssoDb);
-  });
+  // wordAssoDb.connection.on('close', function() {
+  //   console.log('MONGOOSE default connection CLOSED to ' + config.wordAssoDb);
+  // });
 
-  wordAssoDb.connection.on('error', function(err) {
-    console.log("MONGOOSE ERROR\n" + err);
-  });
+  // wordAssoDb.connection.on('error', function(err) {
+  //   console.log("MONGOOSE ERROR\n" + err);
+  // });
 
-  // When the connection is disconnected
-  wordAssoDb.connection.on('disconnected', function() {
-    console.log('MONGOOSE default connection disconnected');
-  });
+  // // When the connection is disconnected
+  // wordAssoDb.connection.on('disconnected', function() {
+  //   console.log('MONGOOSE default connection disconnected');
+  // });
 
   require('../app/models/hashtag.server.model');
   require('../app/models/media.server.model');
