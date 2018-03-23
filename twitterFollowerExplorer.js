6a36e40af0d663f20c35871b899d86ba915da6be
tracycollins <tc@threeceemedia.com>
2018-03-23 10:54:24 -0400

    log

--- twitterFollowerExplorer.js
+++ twitterFollowerExplorer.js
@@ -2649,7 +2649,6 @@ function processUser(threeCeeUser, userIn, lastTweeId, callback) {
     else {
       callback(null, user);
     }
-
   });
 }
 
@@ -2877,6 +2876,7 @@ function updateNetworkFetchFriends(threeCeeUser, callback){
               + "\nEND FETCH:    " + statsObj.user[threeCeeUser].endFetch
               + "\nSTART:        " + statsObj.user[threeCeeUser].friendsProcessStart.format(compactDateTimeFormat)
               + "\nEND:          " + statsObj.user[threeCeeUser].friendsProcessEnd.format(compactDateTimeFormat)
+              + "\nNOW:          " + moment.format(compactDateTimeFormat)
               + "\nELPSD:        " + msToTime(statsObj.user[threeCeeUser].friendsProcessElapsed)
               + "\nNEXT USER:    " + nextUser
               + "\nABORT CURSOR: " + abortCursor
@@ -2901,6 +2901,7 @@ function updateNetworkFetchFriends(threeCeeUser, callback){
               + "\nEND FETCH:    " + statsObj.user[threeCeeUser].endFetch
               + "\nSTART:        " + statsObj.user[threeCeeUser].friendsProcessStart.format(compactDateTimeFormat)
               + "\nEND:          " + statsObj.user[threeCeeUser].friendsProcessEnd.format(compactDateTimeFormat)
+              + "\nNOW:          " + moment.format(compactDateTimeFormat)
               + "\nELPSD:        " + msToTime(statsObj.user[threeCeeUser].friendsProcessElapsed)
               + "\nNEXT USER:    " + nextUser
               + "\nABORT CURSOR: " + abortCursor
