a974d5e9489b8dd7b19fe56e83b3544bfea8b23e
tracycollins <tc@threeceemedia.com>
2020-11-05 14:53:08 -0500

    package

--- twitterFollowerExplorer.js
+++ twitterFollowerExplorer.js
@@ -2701,10 +2701,13 @@ async function initNetworks(params){
       const nnId = nnIds.shift();
 
       const nnObj = bestNetworkHashMap.get(nnId)
+
       if (nnObj.testCycles === undefined){
         console.log(nnObj)
       }
+
       nnObj.testCycles = nnObj.testCycles || 0;
+
       if (nnId !== bestNetworkObj.networkId && nnObj.testCycles > 0){
         bestNetworkHashMap.delete(nnId);
         console.log(chalkLog("TFE | REMOVED NN" 
@@ -2726,6 +2729,8 @@ async function initNetworks(params){
       }
 
     }
+
+    return
   }
 
   await initActivateNetworks(params);
